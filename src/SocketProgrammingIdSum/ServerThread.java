2036c7c2ee6661a6b103daaa644725bb8446f68a
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-11-17 17:46:38 +0600

    Server Thread for socket programming using Threading, Take your ID as Input and sum the digit

---
+++ src/SocketProgrammingIdSum/ServerThread.java
@@ -0,0 +1,27 @@
+package SocketProgrammingIdSum;
+
+import java.io.*;
+import java.net.*;
+public class ServerThread {
+    public static void main(String args[]) throws IOException{
+        ServerSocket handshake = new ServerSocket (8080);
+        System.out.println("Server connected at " + handshake.getLocalPort());
+        System.out.println("Server is connecting\n");
+        System.out.println("Wait for the client\n");
+        while(true){
+            Socket com_socket = handshake.accept();
+            System.out.println("A new client is connected "+ com_socket);
+            DataOutputStream dos = new DataOutputStream(com_socket.getOutputStream());
+
+
+            DataInputStream dis = new DataInputStream(com_socket.getInputStream());
+
+
+            System.out.println("A new thread is assigning");
+            Thread new_tunnel = new ClientHandler(com_socket, dis, dos);
+            new_tunnel.start();
+
+
+        }
+    }
+}
