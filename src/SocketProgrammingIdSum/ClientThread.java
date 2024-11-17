8632c45e4b14185af3730ff30d1a8f20920a057b
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-11-17 17:49:14 +0600

    Client Thread for socket programming using Threading, Take your ID as Input and sum the digit

---
+++ src/SocketProgrammingIdSum/ClientThread.java
@@ -0,0 +1,50 @@
+package SocketProgrammingIdSum;
+
+/* Client Side Code */
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+public class ClientThread {
+    public static void main(String args[]) throws IOException {
+        try {
+            Socket clientsocket = new Socket("localhost", 8080);
+            System.out.println("Connected to server Handshaking port " + clientsocket.getPort());
+            System.out.println("Client is connecting at Communication Port " + clientsocket.getLocalPort());
+            System.out.println("Client is Connected");
+
+
+            Scanner scn = new Scanner(System.in);
+            DataOutputStream dos = new DataOutputStream(clientsocket.getOutputStream());
+            DataInputStream dis = new DataInputStream(clientsocket.getInputStream());
+
+
+            while (true) {
+                String inLine = dis.readUTF();
+                System.out.println(inLine);
+                String outLine = scn.nextLine();
+                dos.writeUTF(outLine);
+
+
+                if (outLine.equalsIgnoreCase("Exit")) {
+                    System.out.println("Closing the connection " + clientsocket);
+                    clientsocket.close();
+                    System.out.println("Connection Closed");
+                    break;
+                }
+
+
+                String received = dis.readUTF();
+                System.out.println(received);
+            }
+
+
+            dos.close();
+            dis.close();
+            clientsocket.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+}
