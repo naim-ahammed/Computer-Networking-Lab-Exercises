833a61be14f2a32fb51b9360f2a262ca8edda3fc
naim-ahammed <125636794+naim-ahammed@users.noreply.github.com>
2024-11-17 12:56:40 +0600

    One wey server socket programming

---
+++ src/OneWaySocketProgramming/ServerOneWay.java
@@ -0,0 +1,35 @@
+package OneWaySocketProgramming;
+
+/* Server Side Code */
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.*;
+
+
+public class ServerOneWay {
+    public static void main(String[] args) throws IOException{
+        ServerSocket ss = new ServerSocket (5000);
+        System.out.println("Server is connected at port no: " + ss.getLocalPort());
+
+
+        System.out.println("Server is connecting\n");
+        System.out.println("Waiting for the client\n");
+        Socket s = ss.accept();
+        System.out.println("Client request is accepted at port no: " + s.getPort());
+
+
+        System.out.println("Server’s Communication Port: "+s.getLocalPort());
+        DataInputStream input = new DataInputStream(s.getInputStream());
+        String str = "";
+
+
+        while(!str.equals("stop")){
+            str = input.readUTF();
+            System.out.println("Client Says: "+str );
+        }
+        s.close();
+        input.close();
+    }
+
+
+}
